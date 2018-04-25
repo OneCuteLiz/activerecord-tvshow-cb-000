7cdb61a2fc50fddf886f520c39aeaba3d270400e
Liz Jenerson <liz.jenerson@gmail.com>
2018-04-25 09:36:22 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,9 +6,31 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     rating = self.highest_rating
-    self.where("rating = ?", rating)
-    self.name
+    self.find_by(rating: rating)
+    # .find_by returns a model object, .where returns an
+    # ActiveRecord::Relation object
+    # http://comedyofexceptions.blogspot.com/2016/03/findby-vs-where.html
   end
 
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    rating = self.lowest_rating
+    self.find_by(rating: rating)
+  end
+
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+
+  def self.popular_shows
+    self.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order(:name)
+  end
 
 end
